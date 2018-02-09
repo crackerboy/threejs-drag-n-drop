bd409fdac191b20792887787ec9255a7b54d7ff1
Teo Si-Yan <teosiyan@hotmail.com>
2018-02-09 11:14:57 +0800

    Add axes and grid helpers

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.set(10, 10, 0);
+camera.lookAt(new THREE.Vector3(0,0,0));
+
+var objects = [];
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-camera.position.z = 1;
+var axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
+var gridHelper = new THREE.GridHelper( 10, 10 );
+scene.add( gridHelper );
 
 var animate = function () {
   requestAnimationFrame(animate);
@@ -14,12 +22,14 @@ var animate = function () {
 
 animate();
 
+// CONTROLS
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.25;
 controls.enableZoom = true;
-
 controls.update();
+
+var dragControls = new THREE.DragControls( objects, camera, renderer.domElement, controls );
 
 // LIGHTS
 hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
@@ -46,32 +56,29 @@ mtlLoader.load('potions.mtl', function (materials) {
   objLoader.load('potions.obj', function (object) {
 
     scene.add(object);
+    objects.push(object);
     object.position.y = 0;
 
   })
 })
 
-var objects = [];
-
-// Add 100 random objects (spheres)
+// Add 10 random objects (spheres)
 var object, material, radius;
 var objGeometry = new THREE.SphereGeometry(1, 24, 24);
-for (var i = 0; i < 50; i++) {
+for (var i = 0; i < 10; i++) {
   material = new THREE.MeshPhongMaterial({color: Math.random() * 0xffffff});
   material.transparent = true;
   object = new THREE.Mesh(objGeometry.clone(), material);
   objects.push(object);
 
-  radius = Math.random() * 4 + 2;
+  radius = Math.random() * 1 + 0.5;
   object.scale.x = radius;
   object.scale.y = radius;
   object.scale.z = radius;
 
-  object.position.x = Math.random() * 50 - 25;
-  object.position.y = Math.random() * 50 - 25;
-  object.position.z = Math.random() * 50 - 25;
+  object.position.x = Math.random() * 5 - 2;
+  object.position.y = Math.random() * 5;
+  object.position.z = Math.random() * 5 - 2;
 
   scene.add(object);
 }
-
-var dragControls = new THREE.DragControls( objects, camera, renderer.domElement, controls );
